5dbfbec1a587528afaa8fac7c26f399b657d9bd0
Vitalii Nagara <duke@ejoom.com>
2014-03-09 10:40:29 +0200

    delete default language by default

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 CKEDITOR.editorConfig = function( config )
 {
 	// Define changes to default configuration here. For example:
-	config.language = 'ru';
+	// config.language = 'ru';
 	// config.uiColor = '#AADC6E';
 	//config.uiColor = '#DDDC6E';
 };
